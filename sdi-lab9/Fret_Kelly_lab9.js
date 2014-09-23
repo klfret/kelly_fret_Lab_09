55be4807da516cd96e950197e133ed8e80411c53
Kelly <KelCin@Kellys-MacBook-Pro.local>
2014-09-23 15:00:08 -0400

    Commit 7 - Lab 09 - Problem 1 - Format Number.

--- sdi-lab9/Fret_Kelly_lab9.js
+++ sdi-lab9/Fret_Kelly_lab9.js
@@ -6,48 +6,48 @@
 // 23 September 2014
 
 
-// Problem 1 - Format a number
+// ***** Problem 1 - Format a number *****
 // globla variables
-var number1 = 566.6471983;
-var number2 = 2;
 
-var formatNumber = function(num1, num2) {
-	
-	// format num1 to 2 decimal places and assign formatted number to num1
-	num1 = num1.toFixed(num2);
-	
-	return num1;
-};
-
-newNumber = formatNumber(number1, number2);
-
-console.log("The formatted number is now: " + newNumber);
+// var number1 = 567.1671983;
+// var number2 = 2;
+// 
+// var formatNumber = function(num1, num2) {
+// 	
+	// // format num1 to 2 decimal places and assign formatted number to num1 and return value
+	// num1 = num1.toFixed(num2);
+// 	
+	// return num1;
+// };
+// 
+// newNumber = formatNumber(number1, number2);
+// 
+// console.log("The formatted number is now: " + newNumber);
 
 
 // **************************************************************************************************************************
 
-// Problem 2 - Number vs. String
-
-
-
-
+// ***** Problem 2 - Number vs. String *****
 // global variables
+
 var myNumber = "617";
 
-
+// if argument passed is a number, convert to a number and return value
 var numberString = function(string) {
 	
+	var numStr;
 	
-	if (isNaN(string)) {
-		
+	if (!isNaN(string)) {
+		numStr = parseFloat(string);
+	} else {
+		numStr = "not a number";
 	}
 	
-	
-	
+	return numStr;
 };
 
-returnedNumber = numberString(myNumber);
-
+returnedValue = numberString(myNumber);
+console.log("The value is: " + returnedValue);
 
 
 
