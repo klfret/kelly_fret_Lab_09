d2502942d4bf215b2e4870e558eccfc9d9724a68
Kelly <KelCin@Kellys-MacBook-Pro.local>
2014-09-23 16:31:07 -0400

    Commit 11 - Lab 09 - Problem 1 - Format Number.

--- sdi-lab9/Fret_Kelly_lab9.js
+++ sdi-lab9/Fret_Kelly_lab9.js
@@ -58,39 +58,44 @@ var firstDate = "05/30/2014";
 var secondDate = "05/28/2014";
 //var string = "days";
 
-
-
-
-
+// compare two dates and return the difference
 var dateDifferences = function(dateArg1, dateArg2, strAgr3) {
 	
+	// local variables
 	var date1;
 	var date2;
 	
+	// convert dates, perform subtraction and assign value to dateDiff
 	date1 = new Date(dateArg1);
 	date2 = new Date(dateArg2);
 	dateDiff = date1 - date2;
 	
-	
-	console.log(date1 + "\n" + date2);
-	
-	
-	if (strAgr3 == "hours") {
-		dateDiff = Math.abs(dateDiff/1000/60/60);
-		console.log(dateDiff);
-		console.log("The difference between the two dates in hours is: " + dateDiff + " hours");
-	} else {
-		dateDiff = Math.abs(dateDiff/1000/60/60/24);
-		console.log(dateDiff);
-		console.log("The difference between the two dates in days is: " + dateDiff + " days");
-	}
-	
-	//return dateDiff;
+	// return dateDiff's value
+	return dateDiff;
 	
 };
 
+// prompt user for input
 string = prompt("Enter either \"hours\" or \"days\" ");
+
+// call function and assign returned value to returnedValue
 returnedValue = dateDifferences(firstDate, secondDate, string);
+
+console.log(date1 + "\n" + date2);
+	
+
+// if user enters hours, calculate for hours
+if (strAgr3 == "hours") {
+	dateDiff = Math.abs(dateDiff/1000/60/60);
+	console.log(dateDiff);
+	console.log("The difference between the two dates in hours is: " + dateDiff + " hours");
+	
+// user enters days, calculate for days
+} else {
+	dateDiff = Math.abs(dateDiff/1000/60/60/24);
+	console.log(dateDiff);
+	console.log("The difference between the two dates in days is: " + dateDiff + " days");
+}
 
 
 
