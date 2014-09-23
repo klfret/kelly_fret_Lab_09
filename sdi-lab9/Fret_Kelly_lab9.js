52193eae8a1754c2e01ea5040de0a5cb53d0983f
Kelly <KelCin@Kellys-MacBook-Pro.local>
2014-09-23 13:40:11 -0400

    Commit 1 - Lab 09 - Problem 1 - Format Number.

--- sdi-lab9/Fret_Kelly_lab9.js
+++ sdi-lab9/Fret_Kelly_lab9.js
@@ -1,2 +1,25 @@
-alert("Lab 9 - Math, Numbers, and Date Objects - JavaScript works! - 23 September 2014");
+// alert("Lab 9 - JavaScript works! - 23 September 2014");
 
+// Kelly L. Fret
+// SDI 1409
+// Lab 9 - Math, Number and Date Objects
+// 23 September 2014
+
+
+// globla variables
+var number1 = 566.6171983;
+var number2 = 2;
+
+
+var formatNumber = function(num1, num2) {
+	
+	
+	
+	
+	
+	
+	
+	
+};
+
+formatNumber(number1, number2);
